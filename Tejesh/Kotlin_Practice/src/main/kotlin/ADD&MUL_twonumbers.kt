5944b466535066f67830ed1ff64f6f9808e8a884
TejeshGavas <tejesh.gavas@springernature.com>
2022-12-22 17:11:50 +0530

    First Commit

---
+++ Tejesh/Kotlin_Practice/src/main/kotlin/ADD&MUL_twonumbers.kt
@@ -0,0 +1,23 @@
+//addition of two integer number
+
+fun main(args: Array<String>) {
+    val add1 = 50            //initialize
+    val add2 = 30
+
+    val sum =add1 + add2   //addition
+
+    println("Addition is: $sum")  //print output
+
+}
+
+//multiplication of two floating numbers
+
+/*
+fun main(args: Array<String>) {
+    val num1 = 1.8f     //initialize
+    val num2 = 3.2f
+
+    val product = num1*num2   //multiplication
+    println("The product is: $product")   //print output
+}
+*/
---
+++ Tejesh/Kotlin_Practice/src/main/kotlin/ASCII value of Char.kt
@@ -0,0 +1,8 @@
+//Find ASCII value of a character
+fun main() {
+    val c = 'a'
+    val ascii = c.toInt()
+
+    print("The ASCII value of $c is: " )
+    println(ascii)
+}
