1948dfc63a52fa89813a317ef5062e2afd64e0cd
trishalmandrik <trishal.mandrik@springernature.com>
2025-08-01 11:34:05 +0530

    coroutines async-await iteration-2

--- Trishal/KotlinCoroutines/src/main/kotlin/Main.kt
+++ Trishal/KotlinCoroutines/src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -9,19 +10,7 @@ fun main() {
     val time = measureTimeMillis {
         runBlocking {
             println("Weather Forecast")
-
-            val forecast: Deferred<String> = async {
-                getForecast()
-            }
-
-            val temperature: Deferred<String> = async {
-                getTemperature()
-            }
-
-            println("Fetching data...")
-            println("Forecast: ${forecast.await()}")
-            println("Temperature: ${temperature.await()}")
-
+            println(getWeatherReport())
             println("Connection closed!")
         }
     }
@@ -36,4 +25,11 @@ suspend fun getForecast(): String {
 suspend fun getTemperature(): String {
     delay(2000)
     return "25°C"
+}
+
+suspend fun getWeatherReport() = coroutineScope {
+    val forecast = async { getForecast() }
+    val temperature = async { getTemperature() }
+
+    "Forecast: ${forecast.await()}, Temperature: ${temperature.await()}"
 }
